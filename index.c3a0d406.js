68b34cb7f3374d24c32d1d4ec1bf5fafa490bf46
MatiushechkoMark <mmatiushechko@gmail.com>
2024-06-11 20:39:50 +0300

    make build

---
+++ index.c3a0d406.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");document.querySelector(".spider").style.position="static",e.style.display="flex",e.style.justifyContent="center",e.style.alignItems="center";
+//# sourceMappingURL=index.c3a0d406.js.map
